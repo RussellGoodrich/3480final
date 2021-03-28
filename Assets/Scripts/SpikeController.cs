eebcc818f821fb0a39e7bce9eb9336bf595312ea
RussellGoodrich <theindustrialdev@gmail.com>
2021-03-28 02:19:35 -0400

    Tutorial3 commit

---
+++ Assets/Scripts/SpikeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeController : MonoBehaviour
+{
+	public AudioClip hitClip;
+	
+    void OnTriggerStay2D(Collider2D other)
+    {
+        RubyController controller = other.GetComponent<RubyController >();
+
+        if (controller != null)
+        {
+			controller.hitEffect.Play();
+            controller.ChangeHealth(-1);
+        }
+    }
+	
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        RubyController controller = other.gameObject.GetComponent<RubyController >();
+
+        if (controller != null)
+        {
+			controller.PlaySound(hitClip);
+        }
+    }
+
+}
